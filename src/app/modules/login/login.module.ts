70f9b471dba83287fd0fae6827854f888ed90b1a
Manuel Del Pozo <pxmdelpo@cibertec.com.edu>
2019-11-02 20:49:02 -0500

    Agregar login form

--- src/app/modules/login/login.module.ts
+++ src/app/modules/login/login.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from "@angular/core";
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
 import { SignInComponent } from './sign-in/sign-in.component';
 import { LoginComponent } from './login.component';
 import { LoginRoutingModule } from './login-routing.module';
@@ -8,6 +10,7 @@ import { LoginRoutingModule } from './login-routing.module';
     declarations: [SignInComponent, LoginComponent], // components, directives, pipes
     imports: [
         CommonModule,
+        FormsModule,
         LoginRoutingModule
     ], // modules
     providers: [] // services
--- src/app/modules/login/sign-in/sign-in.component.ts
+++ src/app/modules/login/sign-in/sign-in.component.ts
@@ -6,10 +6,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./sign-in.component.css']
 })
 export class SignInComponent implements OnInit {
+  model: any;
 
   constructor() { }
 
   ngOnInit() {
+    this.model = {
+      email: '',
+      password: ''
+    };
   }
 
+  onClick(e) {
+    e.preventDefault();
+    console.log(this.model);
+  }
 }
